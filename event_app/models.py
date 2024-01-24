423bc424251dff8a92fb38716f2809d4025ba7a3
Jj RamOS <fakemail@fakeDomain.com>
2024-01-23 19:19:33 -0800

    Udate Data model and Serializers

--- event_app/models.py
+++ event_app/models.py
@@ -1,13 +1,27 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 
+    
+class Room(models.Model):
+    name = models.CharField(max_length=100)
+    chairs = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.name
+
+class Chair(models.Model):
+    room = models.ForeignKey(Room, related_name="chairs", on_delete=models.CASCADE)
+    number = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.room.name} Chair {self.number}"
+    
 class Event(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-    #chair = models.ForeignKey(Chair, on_delete=models.SET_NULL, null=True, blank=True)
-
+    chair = models.ForeignKey(Chair, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -22,21 +36,15 @@ class Event(models.Model):
         if overlapping_events.exists():
             raise ValidationError('There is an overlapping event, please try a different time')
 
+        # Check if the room and chair are available
+        if Event.objects.filter(
+            room=self.room, 
+            chair=self.chair, 
+            start_time__lt=self.end_time, 
+            end_time__gt=self.start_time
+        ).exclude(id=self.id).exists():
+            raise ValidationError('The selected room and chair are not available.')
+
     def save(self, *args, **kwargs):
         self.clean()
         super().save(*args, **kwargs)
-
-    
-class Room(models.Model):
-    name = models.CharField(max_length=100)
-    chairs = models.PositiveIntegerField(default=1)
-
-    def __str__(self):
-        return self.name
-
-class Chair(models.Model):
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
-    number = models.IntegerField()
-
-    def __str__(self):
-        return f"{self.room.name} Chair {self.number}"
--- event_app/serializers.py
+++ event_app/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
-from .models import Event
+from .models import Event, Room, Chair
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = '__all__'
+
+class ChairSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Chair
+        fields = '__all__'
 
 class EventSerializer(serializers.ModelSerializer):
+    room = RoomSerializer(read_only=True)
+    chair = ChairSerializer(read_only=True)
     class Meta:
         model = Event
         fields = '__all__'
