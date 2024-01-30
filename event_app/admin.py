fa2e2e2655cc0e9433d5bd2a2132cb8b78d0cfa4
Jj RamOS <fakemail@fakeDomain.com>
2024-01-30 09:55:25 -0800

    Fix entity relashionship issue - database working ok now

--- event_app/admin.py
+++ event_app/admin.py
@@ -4,16 +4,16 @@ from .models import Room, Chair, Event
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
     pass
-   # list_display = ('name', 'chairs')
+    list_display = ('name', 'pk')
 @admin.register(Chair)
 class ChairAdmin(admin.ModelAdmin):
     pass
-   # list_display = ('room', 'number', 'price', 'isTaken', 'isVIP')
-   # list_filter = ('room', 'isTaken', 'isVIP')
+    list_display = ('room', 'price', 'isTaken', 'isVIP')
+    list_filter = ('room', 'isTaken', 'isVIP')
 
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     pass
-    #list_display = ('room', 'chair', 'start_time', 'end_time')
-    #list_filter = ('room', 'chair')
+    list_display = ('room', 'start_time', 'end_time')
+    list_filter = ('room', 'start_time', 'end_time')    
--- event_app/models.py
+++ event_app/models.py
@@ -2,36 +2,33 @@ from django.db import models
 from django.core.exceptions import ValidationError
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import User
     
 class Room(models.Model):
     name = models.CharField(max_length=100)
-    chairs = models.PositiveIntegerField(default=1)
-
     def __str__(self):
         return self.name
 
 class Chair(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
-    number = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     isTaken = models.BooleanField(default=False)
     isVIP = models.BooleanField(default=False)
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey('content_type', 'object_id')
-
+    
     def __str__(self):
         return f"{self.room.name} Chair {self.number}"
     
 class Event(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-    chair = models.ForeignKey(Chair, on_delete=models.CASCADE, null=True, blank=True)
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    #chair = models.ForeignKey(Chair, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content_type = models.ForeignKey('contenttypes.ContentType', on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey('content_type', 'object_id')
+    content_object = GenericForeignKey()
 
     def __str__(self):
         return self.title
